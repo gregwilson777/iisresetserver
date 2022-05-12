eb1950a2007729fcb902f74e9dee316fefcd6a45
Greg Wilson <79232785+gregwilson777@users.noreply.github.com>
2022-05-12 22:16:55 +1000

    removed comments

--- main.go
+++ main.go
@@ -197,9 +197,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//defer to close when you're done with it, not because you think it's idiomatic!
 	defer f.Close()
-	//set output of logs to f
 	log.SetOutput(f)
 
 	prg := &program{}
